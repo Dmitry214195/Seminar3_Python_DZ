206ce76bbdcda1962f361894b1d60920212dacee
Dmitry Artamonow <dmitri.artamonow@yandex.ru>
2022-12-02 13:47:05 +0500

    Task 5

--- Task_5.py
+++ Task_5.py
@@ -1,7 +1,5 @@
-''' Задача: Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-    Пример:
-        для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
-'''
+# Задание 5: Задайте число. Составьте список чисел Фибоначчи, 
+# в том числе для отрицательных индексов. 
 
 def Fibonacci(n):
     if n in [1, 2]:                       
@@ -21,7 +19,7 @@ def NegaFibonacci(n):
         return num2
 
 list = [0]
-userNumber = int(input('Enter any number: '))
+userNumber = int(input('Введите число: '))
 for e in range(1, userNumber + 1):
     list.append(Fibonacci(e))
     list.insert(0, NegaFibonacci(e))
