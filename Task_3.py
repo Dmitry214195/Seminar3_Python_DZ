7ae34faf83e7ce513307957aa1c4901051c1ed7e
Dmitry Artamonow <dmitri.artamonow@yandex.ru>
2022-12-02 16:04:38 +0500

    Task 3

--- Task_3.py
+++ Task_3.py
@@ -1,9 +1,17 @@
-# Задача №24: Задайте список из вещественных чисел. 
-# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
-# Пример:
-#   [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+# Задание 3: Задайте список из вещественных чисел. 
+# Напишите программу, которая найдёт разницу между максимальным и
+# минимальным значением дробной части элементов.
 
+from random import uniform
 
-lst = list(map(float, input("Введите числа через пробел:\n").split()))
-new_lst = [round(i%1,2) for i in lst if i%1 != 0]
-print(max(new_lst) - min(new_lst))
+number = int(input('Введите размер списка: '))
+list = []
+
+for i in range(number):
+    list.append(uniform(1, 9))
+    list=[round(v,1) for v in list]
+
+print(list)  
+
+list =  [round(i%1,2) for i in list if i%1 != 0]
+print(max(list) - min(list)) 
